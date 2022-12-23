27f7d2d8eeb97224d05e2a199b3032fd7c4c0133
Saba <saba.sadegh67@gmail.com>
2022-12-23 17:59:14 +0330

    display colors dynamiclly

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const url = "https://course-api.com/javascript-store-products";
 
-const productDOM = document.querySelector(".products-center");
+const productsDOM = document.querySelector(".products-center");
 
 const fetchProduct = async () => {
-  productDOM.innerHTML = ` <div class="loading"></div>`;
+  productsDOM.innerHTML = ` <div class="loading"></div>`;
   try {
     const resp = await fetch(url);
     const data = await resp.json();
     return data;
   } catch (error) {
-    productDOM.innerHTML = `<p class="error">there was an error</p>`;
+    productsDOM.innerHTML = `<p class="error">there was an error</p>`;
   }
 };
 
@@ -21,7 +21,6 @@ const displayProduct = (list) => {
         fields: { price, name: title },
       } = product;
       const { url: image } = product.fields.image[0];
-      console.log(id, price, title);
 
       return `<a class="single-product" href="product.html?id=${id}">
     <img src="${image}" class="single-product-img" alt="${title}" />
@@ -32,7 +31,7 @@ const displayProduct = (list) => {
   </a>`;
     })
     .join("");
-  productDOM.innerHTML = `<div class="products-container">${productsList}</div>`;
+  productsDOM.innerHTML = `<div class="products-container">${productsList}</div>`;
 };
 
 const start = async () => {
