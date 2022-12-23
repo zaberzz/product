701d31c34c5786f25bb4ab5d5403116b9f61e94b
Saba <saba.sadegh67@gmail.com>
2022-12-23 12:09:20 +0330

    fetch data from api

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('products starter');
+const url = "https://course-api.com/javascript-store-products";
+
+const productDOM = document.querySelector(".products-center");
+
+const fetchProduct = async () => {
+  productDOM.innerHTML = ` <div class="loading"></div>`;
+  try {
+    const resp = await fetch(url);
+    const data = await resp.json();
+    console.log(data);
+  } catch (error) {
+    productDOM.innerHTML = `<p class="error">there was an error</p>`;
+  }
+};
+fetchProduct();
